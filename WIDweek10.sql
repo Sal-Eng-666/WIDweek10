257fab307224b107717ab074fb78635897a73531
Sal-Eng-666 <76225542+Sal-Eng-666@users.noreply.github.com>
2021-03-19 20:53:40 +0000

    Add files via upload

---
+++ WIDweek10.sql
@@ -0,0 +1,23 @@
+
+insert into samples (account_number, cust_name, forage, harvest_date, sample_date)
+values ('001','Smith','grass', '20/06/02', '21/03/17'),
+('002','Green', 'maize', '20/10/01', '21/03/17'),
+('003', 'Brown', 'wheat wholecrop', '20/08/01', '21/03/18');
+select * from samples;
+ use silage;
+ select * from samples
+ where cust_name = grass;
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
